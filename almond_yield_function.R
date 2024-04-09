0c63cc1143d50b578c27aef043c5d1a176764dcd
ccaswell25 <ccaswell25@gmail.com>
2024-04-09 08:45:16 -0700

    updates to function.r

--- almond_yield_function.R
+++ almond_yield_function.R
@@ -1,6 +1,14 @@
-#function definition
+#' Almond Yield Function
+#' Computes the almond yield anomoly in response to climate
+#' @param area area of panel (m2) NEED TO CHANGE
+#' @param eff solar panel efficiency (0-1) default 0.8 NEED TO CHANGE
+#' @param PR performance ratio (system related) (0-1) default is 0.75 NEED TO CHANGE
+#' @param eunits energy output: J results in kJ/m2/year or W then assume KWh, default = J NEED TO CHANGE
+#' @author Annie Adams and Carly Caswell
+#' @return x (unit), y (unit)
 
-
-
-
-#correct function implementation 
+almond_yield = function() {
+  
+  return()
+  
+}
